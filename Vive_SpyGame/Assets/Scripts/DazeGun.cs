f04391e419ccbbda06da7d21b6c9f30bb9b801a6
lunerfox <lunerfox@gmail.com>
2016-07-11 01:44:08 -0700

    Added a gun to the game - A taser meant to disable droids. Have limited shots Added bullets and bullet count Added a Battery power up - Recharges the gun

---
+++ Vive_SpyGame/Assets/Scripts/DazeGun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DazeGun : MonoBehaviour
+{
+    public Transform BulletSpawnPoint;
+    public GameObject BulletPrefab;
+    public int StartingAmmo;
+    public float BulletSpeed;
+    public SteamVR_TrackedObject trackedObj;
+
+    public int ammoCount { get; set; }
+
+    void Start() {
+        ammoCount = StartingAmmo;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //print("Right Controller has pulled the trigger");
+            if (ammoCount > 0)
+            {
+                GameObject bullet = GameObject.Instantiate(BulletPrefab, BulletSpawnPoint.position, BulletSpawnPoint.rotation) as GameObject;
+                bullet.GetComponent<Rigidbody>().velocity = BulletSpawnPoint.transform.up * BulletSpeed;
+                GetComponent<AudioSource>().Play();
+                //Haptic Feedback
+                device.TriggerHapticPulse(3000);
+                ammoCount--;
+            }
+        }
+    }
+}
---
+++ Vive_SpyGame/Assets/Scripts/UIAmmoUpdate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//To be used with a Gun Object
+
+public class UIAmmoUpdate : MonoBehaviour {
+
+    public GameObject GunObject;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = GunObject.GetComponent<DazeGun>().ammoCount.ToString();
+	}
+}
