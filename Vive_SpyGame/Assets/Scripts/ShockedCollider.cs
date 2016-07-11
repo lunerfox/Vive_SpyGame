0a353723fb17ea5827017e71c270c59bd1d91bd2
lunerfox <lunerfox@gmail.com>
2016-07-11 15:08:20 -0700

    Drones are now disabled when shot. Time can be modified via main drone object Added an electric particle effect when disabled Added sounds for BGM and various gun related things

---
+++ Vive_SpyGame/Assets/Scripts/ShockedCollider.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockedCollider : MonoBehaviour {
+
+    public float shockTime;         //Amount of time to set when the robot is shocked.
+    private float timeTilEnable;    //This time is > 0 when the robot is shocked. Counts down to 0 and enables the robot again
+
+    ParticleSystem shockParticle;
+
+    public GameObject electricBuzz;
+    AudioSource AS_electricBuzz;
+
+    // Use this for initialization
+    void Start()
+    {
+        shockParticle = GetComponentInChildren<ParticleSystem>();
+        AS_electricBuzz = electricBuzz.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeTilEnable > 0) timeTilEnable -= Time.deltaTime;
+        else
+        {
+            GetComponent<NavMeshAgent>().Resume();
+            shockParticle.Stop();
+            AS_electricBuzz.Stop();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Drone Hit by " + collision.collider.gameObject.name);
+        GetComponent<NavMeshAgent>().Stop();
+        timeTilEnable = shockTime;
+        shockParticle.Play();
+        AS_electricBuzz.Play();
+        //Instantiate(Explosion, transform.position, Quaternion.identity);
+        Destroy(collision.collider.gameObject);
+    }
+}
